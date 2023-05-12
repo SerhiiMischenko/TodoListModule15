13f6348b27b6d556ec6ff0f35c521845be9b0256
SerhiiMischenko <serj.roshen@gmail.com>
2023-05-12 13:12:53 +0300

    Rework Spring project to without configuration.xml file

---
+++ src/main/java/com/example/todoList/SpringConfig.java
@@ -0,0 +1,11 @@
+package com.example.todoList;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.example.todoList")
+public class SpringConfig {
+
+}
