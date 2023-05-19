4657af62ed11d693b39b2919f28dbb49e8e956fe
SerhiiMischenko <serj.roshen@gmail.com>
2023-05-19 12:06:09 +0300

    Upd view list.html(add button create)

--- src/main/java/com/example/todoList/mvc/NoteController.java
+++ src/main/java/com/example/todoList/mvc/NoteController.java
@@ -20,18 +20,13 @@ public class NoteController {
 
     @GetMapping("/list")
     public ModelAndView getAllNotes() {
-        System.out.println("In getAllNotes()");
         ModelAndView result = new ModelAndView("list");
         result.addObject("list", noteService.listAll());
         return result;
     }
     @PostMapping("/delete")
     public String deleteNote(@RequestParam("id") Long id) {
-        System.out.println("In delete");
-        System.out.println(id);
-        System.out.println(noteService);
         noteService.deleteById(id);
-        System.out.println(noteService);
         return "redirect:/note/list";
     }
 }
