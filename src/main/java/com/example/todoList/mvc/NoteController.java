73d57a360618d6727612e4596c056e4c5e3932f6
SerhiiMischenko <serj.roshen@gmail.com>
2023-05-19 12:56:36 +0300

    Create view create.html, edit html

--- src/main/java/com/example/todoList/mvc/NoteController.java
+++ src/main/java/com/example/todoList/mvc/NoteController.java
@@ -1,32 +1,56 @@
 package com.example.todoList.mvc;
 
+import com.example.todoList.Note;
 import com.example.todoList.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/note")
 public class NoteController {
     private final NoteService noteService;
-@Autowired
+
+    @Autowired
     public NoteController(NoteService noteService) {
         this.noteService = noteService;
     }
 
     @GetMapping("/list")
-    public ModelAndView getAllNotes() {
-        ModelAndView result = new ModelAndView("list");
-        result.addObject("list", noteService.listAll());
-        return result;
+    public String getAllNotes(Model model) {
+        model.addAttribute("list", noteService.listAll());
+        return "list";
     }
+
     @PostMapping("/delete")
     public String deleteNote(@RequestParam("id") Long id) {
         noteService.deleteById(id);
+        return "redirect:/note/list";
+    }
+
+    @GetMapping("/create")
+    public String createNoteForm(Model model) {
+        model.addAttribute("note", new Note());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String createNoteSubmit(@ModelAttribute("note") Note note) {
+        noteService.add(note);
+        return "redirect:/note/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editNoteForm(@PathVariable("id") Long id, Model model) {
+        Note note = noteService.getById(id);
+        model.addAttribute("note", note);
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String editNoteSubmit(@ModelAttribute("note") Note note) {
+        noteService.update(note);
         return "redirect:/note/list";
     }
 }
